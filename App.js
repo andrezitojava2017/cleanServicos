52cc016e1640210795a426b25d17be4db94c1320
Jederson Andre <andre_sjx@live.com>
2022-11-03 22:25:14 -0300

    adicionado as permissoes android de localização

--- App.js
+++ App.js
@@ -5,15 +5,17 @@ import Login from './screens/Login/Login';
 import Profile from './screens/profile/Profile';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import UserLocation from './screens/Locations/UserLocation';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="ListUsers">
+      <Stack.Navigator initialRouteName="Location">
         <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="ListUsers" component={ListUsers} />
+        <Stack.Screen name="Location" component={UserLocation} />
       </Stack.Navigator>
       {/*<Login /> 
       < />
