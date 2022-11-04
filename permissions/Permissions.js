3cc722823124f06c342156dab5a615e32c0067b0
Jederson Andre <andre_sjx@live.com>
2022-11-03 22:23:09 -0300

    criação da função para solicitar permissoes ao usuario

---
+++ permissions/Permissions.js
@@ -0,0 +1,23 @@
+import {Alert, PermissionsAndroid} from 'react-native';
+const RequestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Permissao de acesso a localização',
+        buttonNeutral: 'Pergunta-me depois',
+        buttonNegative: 'Cancelar',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      return true;
+    } else {
+      Alert.alert('Acesso Negado ', 'Permissão negada');
+      return false;
+    }
+  } catch (err) {
+    Alert.alert('Error ', err);
+  }
+};
+export default RequestLocationPermission;
