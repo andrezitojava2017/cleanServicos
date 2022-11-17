f5e1a7680175dba9068450dedde78a4224435b90
Jederson Andre <andre_sjx@live.com>
2022-11-16 22:39:59 -0300

    Tentativa de implementação para inserir markers de testes no mapa. inclusao de array com dados de markers ficticios e localização

--- screens/Locations/UserLocation.js
+++ screens/Locations/UserLocation.js
@@ -2,12 +2,40 @@ import React, {useEffect, useState} from 'react';
 import RequestPermission from '../../permissions/Permissions';
 import Geolocation from 'react-native-geolocation-service';
 import MapView, {Marker} from 'react-native-maps';
-import LocationUsers from '../../locationsUsers';
 import {Platform} from 'react-native';
 
 const UserLocation = props => {
   const [slatitude, setSlatitude] = useState(0);
   const [slongitude, setSlongitude] = useState(0);
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      name: 'Primary Item',
+      coords: {
+        latitude: slatitude + -0.002,
+        longitude: slongitude + -0.002,
+      },
+      service: ['Faxina em geral', 'Lavar roupas', 'Passar roupas'],
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      name: 'Second Item',
+      coords: {
+        latitude: slatitude + -0.003,
+        longitude: slongitude + -0.00012,
+      },
+      service: ['Lavar roupas', 'Passar roupas'],
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      name: 'Third Item',
+      coords: {
+        latitude: slatitude + -0.002,
+        longitude: slongitude + -0.00012,
+      },
+      service: ['Lavar roupas'],
+    },
+  ];
 
   useEffect(() => {
     (async () => {
@@ -41,12 +69,6 @@ const UserLocation = props => {
     });
   };
 
-  const handleSelect = e => {
-    let coordinate = e.nativeEvent.coordinate;
-    console.log({...coordinate});
-    props.navigation.navigate('Profile', {...coordinate});
-  };
-
   return (
     <MapView
       style={{width: '100%', height: '100%'}}
@@ -58,33 +80,37 @@ const UserLocation = props => {
         longitudeDelta: 0.00425,
       }}
       showsUserLocation={true}>
-      {slatitude != 0 ? (
+      {slatitude != 0
+        ? DATA.map(marker => {
+            <Marker
+              key={marker.id}
+              onPress={e => {
+                props.navigation.navigate('Profile', {
+                  ...marker,
+                });
+              }}
+              coordinate={{...marker.coords}}
+              title={`${marker.name}`}
+            />;
+          })
+        : /*
         <>
           <Marker
-            onPress={handleSelect}
+            onPress={e => {
+              props.navigation.navigate('Profile', {
+                name: 'Loren Ipson Ipson',
+                ...e.nativeEvent.coordinate,
+              });
+            }}
             coordinate={{
               latitude: slatitude + 0.001,
               longitude: slongitude + 0.0012,
             }}
             title={`Lorem Ipson Ipson`}
           />
-
-          <Marker
-            coordinate={{
-              latitude: slatitude + -0.002,
-              longitude: slongitude + -0.002,
-            }}
-            title={`Fulano de Tal`}
-          />
-          <Marker
-            coordinate={{
-              latitude: slatitude + -0.003,
-              longitude: slongitude + -0.00012,
-            }}
-            title={`Ciclano de tal e tal`}
-          />
         </>
-      ) : null}
+        */
+          alert('nao carregou')}
     </MapView>
   );
 };
