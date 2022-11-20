9b6f0c1c5740be261cda07802810af0d9829c6e0
Jederson Andre <andre_sjx@live.com>
2022-11-20 10:57:32 -0300

    Remoção de imports nao utilizados; Inclusao de novos componentes na screan e implementação de estilo

--- components/Information.js
+++ components/Information.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import color from '../styles/colors.json';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 const Information = props => {
   return (
-    <View>
-      <Icon name="info" size={30} color="#900" />
-      <View style={style.information}>
-        <Text>{props.data}</Text>
+    <View style={style.container}>
+      <Icon name="my-location" size={30} color={color.colors.red} />
+      <View>
+        <Text>Latitude: {props.data.latitude}</Text>
+        <Text>Longitude: {props.data.longitude}</Text>
       </View>
     </View>
   );
 };
 
 const style = StyleSheet.create({
-  information: {
+  container: {
+    display: 'flex',
     backgroundColor: '#D9D9D9',
-    marginRight: 12,
-    marginLeft: 12,
-    marginTop: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 12,
     padding: 12,
+    borderRadius: 18,
+  },
+  information: {
+    padding: 8,
   },
 });
 export default Information;
