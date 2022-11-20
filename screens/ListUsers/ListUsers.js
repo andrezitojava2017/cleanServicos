e955f9bb7b423246564ebf175c946d7f6b3e2c93
Jederson Andre <andre_sjx@live.com>
2022-11-20 10:58:44 -0300

    remoção de variavel  nao utilizados

--- screens/ListUsers/ListUsers.js
+++ screens/ListUsers/ListUsers.js
@@ -4,21 +4,6 @@ import ItemUser from '../../components/ItemUser';
 import styled from 'styled-components/native';
 import color from '../../styles/colors.json';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Jederson Andre dos Santos Santana',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
-
 const Container = styled.TouchableOpacity`
   display: flex;
   flex-direction: row;
