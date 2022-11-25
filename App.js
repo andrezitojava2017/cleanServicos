c52a9984eb4e461cc39812cbc28a8c85cdc939e1
Jederson Andre <andre_sjx@live.com>
2022-11-24 22:55:30 -0300

    Inclusao de estilo na status bar

--- App.js
+++ App.js
@@ -4,30 +4,52 @@ import Profile from './screens/profile/Profile';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import UserLocation from './screens/Locations/UserLocation';
+import colors from './styles/colors.json';
+import {NativeBaseProvider} from 'native-base';
 
 const Stack = createNativeStackNavigator();
 
+const styleStatusBar = {
+  headerStyle: {
+    backgroundColor: `${colors.colors.blue}`,
+  },
+  headerShadowVisible: false,
+  headerTitleAlign: 'center',
+  headerTintColor: '#FFFFFF',
+};
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Location">
-        <Stack.Screen
-          name="Profile"
-          options={{title: 'Perfil'}}
-          component={Profile}
-        />
-        <Stack.Screen
-          name="ListUsers"
-          options={{title: 'Proximos a você'}}
-          component={ListUsers}
-        />
-        <Stack.Screen
-          name="Location"
-          options={{title: 'Mapa'}}
-          component={UserLocation}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Location">
+          <Stack.Screen
+            name="Profile"
+            options={{
+              title: 'Perfil',
+              ...styleStatusBar,
+            }}
+            component={Profile}
+          />
+          <Stack.Screen
+            name="ListUsers"
+            options={{
+              title: 'Disponiveis em sua região',
+              ...styleStatusBar,
+            }}
+            component={ListUsers}
+          />
+          <Stack.Screen
+            name="Location"
+            options={{
+              title: 'Mapa',
+              ...styleStatusBar,
+            }}
+            component={UserLocation}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 };
 
