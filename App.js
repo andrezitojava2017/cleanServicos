f2fc619fa563eacd7382e4c363ca6768937bef33
Jederson Andre <andre_sjx@live.com>
2022-12-11 15:30:58 -0300

    inclusao de bibliotecas redux tookit e apollo client

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ListUsers from './screens/ListUsers/ListUsers';
 import Profile from './screens/profile/Profile';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -10,6 +9,8 @@ import Contract from './screens/contract/Contract';
 import Users from './screens/ListUsers/Users';
 import Register from './screens/Register/Register';
 import Login from './screens/Login/Login';
+import {ApolloProvider} from '@apollo/client';
+import client from './api/clientGraphql/client';
 const Stack = createNativeStackNavigator();
 
 const styleStatusBar = {
@@ -24,60 +25,62 @@ const styleStatusBar = {
 
 const App = () => {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen
-            name="Profile"
-            options={{
-              title: 'Perfil',
-              ...styleStatusBar,
-            }}
-            component={Profile}
-          />
-          <Stack.Screen
-            name="ListUsers"
-            options={{
-              title: 'Disponiveis em sua região',
-              ...styleStatusBar,
-            }}
-            component={Users}
-          />
-          <Stack.Screen
-            name="Location"
-            options={{
-              title: 'Mapa',
-              ...styleStatusBar,
-            }}
-            component={UserLocation}
-          />
-          <Stack.Screen
-            name="Contract"
-            options={{
-              title: '',
-              ...styleStatusBar,
-            }}
-            component={Contract}
-          />
-          <Stack.Screen
-            name="Register"
-            options={{
-              title: 'Cadastre-se',
-              ...styleStatusBar,
-            }}
-            component={Register}
-          />
-          <Stack.Screen
-            name="Login"
-            options={{
-              title: null,
-              ...styleStatusBar,
-            }}
-            component={Login}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <ApolloProvider client={client}>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+              name="Profile"
+              options={{
+                title: 'Perfil',
+                ...styleStatusBar,
+              }}
+              component={Profile}
+            />
+            <Stack.Screen
+              name="ListUsers"
+              options={{
+                title: 'Disponiveis em sua região',
+                ...styleStatusBar,
+              }}
+              component={Users}
+            />
+            <Stack.Screen
+              name="Location"
+              options={{
+                title: 'Mapa',
+                ...styleStatusBar,
+              }}
+              component={UserLocation}
+            />
+            <Stack.Screen
+              name="Contract"
+              options={{
+                title: '',
+                ...styleStatusBar,
+              }}
+              component={Contract}
+            />
+            <Stack.Screen
+              name="Register"
+              options={{
+                title: 'Cadastre-se',
+                ...styleStatusBar,
+              }}
+              component={Register}
+            />
+            <Stack.Screen
+              name="Login"
+              options={{
+                title: null,
+                ...styleStatusBar,
+              }}
+              component={Login}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </ApolloProvider>
   );
 };
 
