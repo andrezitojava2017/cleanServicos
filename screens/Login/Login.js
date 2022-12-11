5ef4af73c54c94201aa1f5cc70f3bbb42cb1af85
Jederson Andre <andre_sjx@live.com>
2022-12-11 15:34:10 -0300

    inclusa de rota

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -1,8 +1,8 @@
-import React, {TouchableOpacity} from 'react-native';
+import React, {TouchableOpacity, useEffect} from 'react-native';
 import {Box, Image, Text, VStack} from 'native-base';
 import colors from '../../styles/colors.json';
 
-const Login = () => {
+const Login = ({navigation}) => {
   return (
     <VStack
       flex="1"
@@ -13,14 +13,16 @@ const Login = () => {
           width="200"
           height="100"
           source={require('../../assets/image/clean-services.png')}
+          alt="Clean Serviços"
         />
       </Box>
       <Box>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Location')}>
           <Box
             backgroundColor={colors.colors.red2}
             marginX="20"
             marginY="8"
+            alignItems="center"
             borderRadius="8">
             <Text
               fontFamily="WorkSans-Reguar"
