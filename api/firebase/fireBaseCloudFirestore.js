75c9de7ad0657fdfdb1b80f5b4b0d287576fa2ca
Jederson Andre <andre_sjx@live.com>
2023-03-08 21:35:13 -0300

    criado função de consulta de serviços de um usuario selecionado no mapa

--- api/firebase/fireBaseCloudFirestore.js
+++ api/firebase/fireBaseCloudFirestore.js
@@ -58,4 +58,30 @@ const readCollectionUser = async () => {
   }
 };
 
-export default {createNewUser, saveServiceOfUser, readCollectionUser};
+const searchServicesUsers = async uid => {
+  let services = [];
+  try {
+    // recuperar serviços disponivel pelo usuario
+    let value = await firestore()
+      .collection('service')
+      .where('uid_user', '==', uid)
+      .get();
+
+    value.forEach(documentSnapshot => {
+      services.push(documentSnapshot.data());
+    });
+  } catch (error) {
+    MessageToast({
+      message: 'Ocorreu um erro na leitura dos serviços',
+      color: 'error.500',
+    });
+  }
+  return services;
+};
+
+export default {
+  createNewUser,
+  saveServiceOfUser,
+  readCollectionUser,
+  searchServicesUsers,
+};
